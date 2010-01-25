ed63fb85d4602d6726b39c4c16b31f103b995ffe
Raghul <raghul@alabaster.(none)>
2010-01-25 17:34:41 +0530

    Added UI changes and milestone issues

--- db/migrate/20100123111917_add_milestone_id_to_ticket.rb
+++ db/migrate/20100123111917_add_milestone_id_to_ticket.rb
@@ -1,6 +1,6 @@
 class AddMilestoneIdToTicket < ActiveRecord::Migration
   def self.up
-    add_column :tickets, :milestone_id, :integer, :conditions => "CONSTRAINTS fk_milestone_id REFERENCES milestones(id)"
+    add_column :tickets, :milestone_id, :integer, :conditions => "CONSTRAINTS fk_milestone_id REFERENCES milestones(id)", :default => 0
   end
 
   def self.down
